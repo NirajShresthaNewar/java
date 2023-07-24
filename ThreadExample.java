35a653081fae735b39ea6a67e9eabd9efa208b32
NirajShresthaNewar <136328579+NirajShresthaNewar@users.noreply.github.com>
2023-07-24 16:00:26 +0545

    Add files via upload

--- ThreadExample.java
+++ ThreadExample.java
@@ -1,31 +1,40 @@
-import java.lang.*;
-class ThreadExample extends Thread{
-    public void run()
-    {
-        System.out.println("inside run method");
+class Tle{
+    void printTle(int n){
+        for(int i=1;i<=5;i++){
+        System.out.println(n*i);
+    }
+        try{
+            Thread.sleep(400);
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+        }
     }
 
-    public static void main(String[]args){
-        ThreadExample t1= new ThreadExample();
-        ThreadExample t2= new ThreadExample();
-        ThreadExample t3= new ThreadExample();
-        System.out.println("t1 thread priority"+t1.getPriority());
-        System.out.println("t2 thread priority"+t2.getPriority());
-        System.out.println("t3 thread priority"+t3.getPriority());
-        t1.setPriority(2);
-        t2.setPriority(5);
-        t3.setPriority(8);
-        //t3.getPriority(21);
-        System.out.println("t1 thread priority"+t1.getPriority());
-        System.out.println("t2 thread priority"+t2.getPriority());
-        System.out.println("t3 thread priority"+t3.getPriority());
-        
-        System.out.println("Currently executing thread:" +Thread.currentThread().getName());
-        System.out.println("Main thread priority:" +Thread.currentThread().getPriority());
-        Thread.currentThread().setPriority(10);
-        System.out.println("MAin thread priority:"+Thread.currentThread().getPriority());
-
+class MyThread1 extends Thread{
+    Tle t;
+    MyThread1(Tle t){
+        this.t=t;
+    }
+    public void run(){
+        t.printTle(5);
     }
 }
-
-
+class MyThread2 extends Thread{
+    Tle t;
+    MyThread2(Tle t){
+        this.t=t;
+    }
+    public void run(){
+        t.printTle(100);
+    }
+}
+ public class ThreadExample{
+    public static void main(String[]args){
+        Tle obj= new Tle();
+        MyThread1 t1= new MyThread1(obj);
+        MyThread2 t2= new MyThread2(obj);
+        t1.start();
+        t2.start();
+    }
+}
